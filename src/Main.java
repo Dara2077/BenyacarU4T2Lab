e4ac61a8d866001fd300e8247062c8277e9238e5
dara2077 <dbenyacar1@gmail.com>
2022-11-14 09:28:34 -0500

    Work from class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        int i = 5;
+        while (i <10){
+            System.out.print(i + " ");
+            i ++;
+        }
+    }
 }
